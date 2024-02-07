23cc4e318c571ef1637112760f47299669ec1763
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2024-02-07 20:11:55 -0300

    mobile att

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,33 +4,38 @@ import { TypeAnimation } from 'react-type-animation';
 
 function Main() {
     return(
-        <div className=' bg-primary-100'>
-            <div className='container max-w-full flex items-center justify-center'>
-                <img className='w-40 pt-12 pb-10 md:pt-16 md:w-64 lg:pt-14' src={FmbName} alt="Logo Willian Dev" />
+        <div className='bg-primary-100 lg:h-auto'>
+            <div className='container max-w-full h-2/5 flex items-center justify-center'>
+                <img className='w-52 pt-12 pb-10 md:pt-16 md:w-64 lg:pt-14' src={FmbName} alt="Logo Willian Dev" />
             </div>
 
             <section className='inline max-w-full lg:flex items-center columns-2'>
-                <h1 className='w-2/2 md:w-1/2 ml-3 lg:ml-40 text-white text-4xl md:text-7xl font-extrabold'>
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to to-pink-500'>Olá, eu sou{" "}</span>
-                    <br />
-                <TypeAnimation
-                sequence={[
-                    'Willian!',
-                    1000,
-                    'Web Developer',
-                    1000,
-                    'Mobile Developer',
-                    1000,
-                    'UI/UX Designer',
-                    1000
-                ]}
-                wrapper="span"
-                speed={50}
-                repeat={Infinity}
-                />
-                </h1>
-
-                <img className='w-2/2 md:w-1/2' src={EdMain} alt="Ed" />
+                <div className='md:w-1/2 h-1/5 ml-3 lg:ml-40 flex items-center'>
+                    <h1 className=' text-white text-4xl md:text-7xl font-staubach'>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-gray-100 to to-gray-400'>Olá, eu sou{" "}</span>
+                        <br />
+                    <TypeAnimation
+                    sequence={[
+                        'Willian!',
+                        1000,
+                        'Web Developer',
+                        1000,
+                        'Mobile Developer',
+                        1000,
+                        'UI/UX Designer',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                    />
+                    </h1>
+                </div>
+                
+                <div className='flex mt-4 h-2/5 md:w-1/2 md:mt-0 items-end'>
+                    <img src={EdMain} alt="Ed"/>
+                </div>
+                
             </section>
         </div>
     )
